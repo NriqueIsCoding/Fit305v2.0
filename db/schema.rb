dacd1d929bd1e126bd4627b9d6956b3940f23855
nrique <eesta83@gmail.com>
2018-09-06 18:17:14 -0400

    dependencies errors

--- db/schema.rb
+++ db/schema.rb
@@ -37,13 +37,6 @@ ActiveRecord::Schema.define(version: 20180204193031) do
     t.index ["instructor_id"], name: "index_courses_on_instructor_id"
   end
 
-  create_table "courses_customers", id: false, force: :cascade do |t|
-    t.bigint "customer_id"
-    t.bigint "course_id"
-    t.index ["course_id"], name: "index_courses_customers_on_course_id"
-    t.index ["customer_id"], name: "index_courses_customers_on_customer_id"
-  end
-
   create_table "customer_courses", force: :cascade do |t|
     t.bigint "customer_id"
     t.bigint "course_id"
@@ -76,6 +69,6 @@ ActiveRecord::Schema.define(version: 20180204193031) do
   end
 
   add_foreign_key "courses", "instructors"
-  add_foreign_key "courses_customers", "courses"
-  add_foreign_key "courses_customers", "customers"
+  add_foreign_key "customer_courses", "courses"
+  add_foreign_key "customer_courses", "customers"
 end
